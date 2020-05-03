2a85a7235ea33b17076e4f3074450f97a486bea5
Solomon APPIER-SIGN <braasig@gmail.com>
2020-05-03 13:05:35 +0000

    created sidebar component

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,26 @@
+<template>
+    <aside class="border-right col-1">
+        <ul class="d-flex flex-column justify-content-between text-center">
+            <li class="nav-item">
+                <a class="nav-link text-dark" href="#">Why us? <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <router-link to="/" class="nav-link text-dark" href="#">Pricing</router-link>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link text-dark" href="#">Plugins</a>
+            </li>
+        </ul>
+    </aside>
+</template>
+
+<script>
+    export default {
+        name: "Sidebar"
+    }
+</script>
+
+<style scoped>
+
+</style>
