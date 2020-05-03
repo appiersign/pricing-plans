5716e93e230389198efa17f7e860548a8711cd8c
Solomon APPIER-SIGN <braasig@gmail.com>
2020-05-03 13:07:11 +0000

    added vue router added pricing and subscribe components

--- src/main.js
+++ src/main.js
@@ -1,14 +1,29 @@
 import Vue from 'vue'
 import App from './App.vue'
 import { makeServer } from "./api/server"
+import VueRouter from "vue-router";
+import Pricing from "./pages/Pricing";
+import Subscribe from "./pages/Subscribe";
 
 
 Vue.config.productionTip = false
+
+Vue.use(VueRouter);
 
 if (process.env.NODE_ENV === "development") {
   makeServer()
 }
 
+const routes = [
+  { path: '/', component: Pricing },
+  { path: '/subscribe', component: Subscribe }
+]
+
+const router = new VueRouter({
+  routes // short for `routes: routes`
+})
+
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
